e091e19e196327aa4400c8154b75a67179b33880
venu124 <venu.kiran.124@gmail.com>
2021-05-18 11:05:06 +1000

    Updated response return type
    Does not really need to return anything since program only prints the response as of now.

--- src/api/cli.py
+++ src/api/cli.py
@@ -10,12 +10,11 @@ app = typer.Typer()
 
 
 @app.command()
-def add_task(name: str = typer.Option(...), comment: str = typer.Option(...)) -> object:
+def add_task(name: str = typer.Option(...), comment: str = typer.Option(...)):
     """Adds a task to your todo list"""
     print("Adding your task...")
     response = api.create_task(name, comment)
     print(response)
-    return response
 
 
 @app.command()
@@ -25,12 +24,11 @@ def list_all(
     completed: Optional[bool] = typer.Option(False),
     comment: Optional[str] = typer.Option(""),
     limit: Optional[str] = typer.Option(""),
-) -> object:
+):
     """Lists all or some of your tasks based on your selected option"""
     print("Listing your task(s) " + str(id))
     response = api.get_tasks(id, name, completed, comment, limit)
     print(response)
-    return response
 
 
 @app.command()
@@ -39,27 +37,24 @@ def update_task(
     name: str = typer.Option(...),
     completed: Optional[bool] = typer.Option(False),
     comment: str = typer.Option(...),
-) -> object:
+):
     """Updates selected task in your todo list"""
     print("Updating your task...")
     response = api.update_task(id, name, completed, comment)
     print(response)
-    return response
 
 
 @app.command()
-def delete_task(id: str = typer.Option(...)) -> object:
+def delete_task(id: str = typer.Option(...)):
     """Deletes selected task from your todo list"""
     print("Deleting your task...")
     response = api.delete_task(id)
     print(response)
-    return response
 
 
 @app.command()
-def mark_as_done(id: str = typer.Option(...)) -> object:
+def mark_as_done(id: str = typer.Option(...)):
     """Marks a particular task as completed"""
     print("Marking your task as done...")
     response = api.mark_task_as_done(id)
     print(response)
-    return response
