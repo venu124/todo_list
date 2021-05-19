520e8a5017f142c81a7e65e899711ba61f1735c9
venu124 <venu.kiran.124@gmail.com>
2021-05-20 00:33:40 +1000

    Formatted response of all functions and removed duplicate params in update_task function

--- src/api/api.py
+++ src/api/api.py
@@ -1,5 +1,6 @@
 # Use this file to add in functions for your programs functionality
 
+import json
 from typing import Optional
 
 import requests
@@ -19,12 +20,12 @@ def create_task(name: str, comment: str) -> str:
     """
     :param name: This parameter accepts a string and contains the task name
     :param comment: This parameter accepts a string and contains the task comment
-    :return: Returns a JSON response in case of SUCCESS OR an error  message in case if a Failure
+    :return: Returns a string which contains success or failure response based on the API response status
     """
     response = requests.post(BASE_URL + BASE_PATH + "/todo", json={"name": name, "comment": comment})
     if response.status_code == 201:
         print("Created your task successfully")
-        return response.text
+        return json.dumps(response.json(), indent=4)
     else:
         return build_error_response(response.text)
 
@@ -38,12 +39,12 @@ def get_tasks(
     :param completed: This optional parameter accepts a boolean and response is filtered based on this value
     :param comment: This optional parameter accepts a string and response is filtered based on this value
     :param limit: This optional parameter accepts a string and response is filtered based on this value
-    :return: Returns a JSON response in case of SUCCESS OR an error  message in case if a Failure
+    :return: Returns a string which contains success or failure response based on the API response status
     """
     response = requests.get(BASE_URL + BASE_PATH + "/todo/" + str(id))
     if response.status_code == 200:
         print("Here is your task(s) list:")
-        return response.text
+        return json.dumps(response.json(), indent=4)
     else:
         return build_error_response(response.text)
 
@@ -54,15 +55,14 @@ def update_task(id: str, name: str, completed: Optional[bool], comment: str,) ->
     :param name: This parameter accepts a string and its value is used to update the task name
     :param completed: This parameter accepts a boolean and its value is used to make the task as completed
     :param comment: This parameter accepts a string and its value is used to update the task comment
-    :return: Returns a JSON response in case of SUCCESS OR an error  message in case if a Failure
+    :return: Returns a string which contains success or failure response based on the API response status
     """
     response = requests.put(
-        BASE_URL + BASE_PATH + "/todo/" + id,
-        json={"name": name, "name": name, "completed": completed, "comment": comment},
+        BASE_URL + BASE_PATH + "/todo/" + id, json={"name": name, "completed": completed, "comment": comment},
     )
     if response.status_code == 200:
         print("Updated your task successfully")
-        return response.text
+        return json.dumps(response.json(), indent=4)
     else:
         return build_error_response(response.text)
 
@@ -70,12 +70,12 @@ def update_task(id: str, name: str, completed: Optional[bool], comment: str,) ->
 def delete_task(id: str) -> str:
     """
     :param id: This parameter accepts a string and its value is used to delete the task
-    :return: Returns a JSON response in case of SUCCESS OR an error  message in case if a Failure
+    :return: Returns a string which contains success or failure response based on the API response status
     """
     response = requests.delete(BASE_URL + BASE_PATH + "/todo/" + id)
     if response.status_code == 200:
         print("Deleted your task successfully")
-        return response.text
+        return json.dumps(response.json(), indent=4)
     else:
         return build_error_response(response.text)
 
@@ -83,11 +83,11 @@ def delete_task(id: str) -> str:
 def mark_task_as_done(id: str) -> str:
     """
     :param id: This parameter accepts a string and its value is used to mark the task as completed
-    :return: Returns a JSON response in case of SUCCESS OR an error  message in case if a Failure
+    :return: Returns a string which contains success or failure response based on the API response status
     """
     response = requests.put(BASE_URL + BASE_PATH + "/todo/" + id, json={"completed": True})
     if response.status_code == 200:
         print("Marked your task as completed")
-        return response.text
+        return json.dumps(response.json(), indent=4)
     else:
         return build_error_response(response.text)
